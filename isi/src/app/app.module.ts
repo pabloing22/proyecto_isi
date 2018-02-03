7f0e3d502e25ff13bb6012a527970bf3bf4bc362
cirkuit93 <cirkuit92@gmail.com>
2018-02-03 21:42:55 +0100

    error
    de pull

--- isi/src/app/app.module.ts
+++ isi/src/app/app.module.ts
@@ -4,14 +4,48 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 
+import { BanerComponent } from './baner/baner.component';
+
+
+import { NavbarComponent } from './navbar/navbar.component';
+import { InicioComponent } from './inicio/inicio.component';
+/*ROUTING */
+import { Routes, RouterModule } from '@angular/router';
+import { NosotrosComponent } from './nosotros/nosotros.component';
+
+
+/*ROUTING */
+const appRoutes: Routes =[
+  {path: '', redirectTo: '/inicio', pathMatch: 'full'},
+  {path: 'inicio', component: InicioComponent},
+  {path: 'nosotros', component: NosotrosComponent},
+
+];
+
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+
+
+    BanerComponent,
+
+    NavbarComponent,
+    InicioComponent,
+    NosotrosComponent
+
+
   ],
+
+
   imports: [
-    BrowserModule
+    /*ROUTING */
+    RouterModule.forRoot(appRoutes),
+    BrowserModule,
   ],
+
+
   providers: [],
   bootstrap: [AppComponent]
 })

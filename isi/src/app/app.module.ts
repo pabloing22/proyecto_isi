9a62488cef5dde88988383cb06d4d7356b71d3f5
pabloing22 <pabloing199364gmail.com>
2018-02-03 14:49:57 -0300

    corregido

--- isi/src/app/app.module.ts
+++ isi/src/app/app.module.ts
@@ -1,32 +1,12 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
-
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './navbar/navbar.component';
 import { InicioComponent } from './inicio/inicio.component';
 /*ROUTING */
 import { Routes, RouterModule } from '@angular/router';
 import { NosotrosComponent } from './nosotros/nosotros.component';
-
-
-/*ROUTING */
-const appRoutes: Routes =[
-  {path: '', redirectTo: '/inicio', pathMatch: 'full'},
-  {path: 'inicio', component: InicioComponent},
-  {path: 'nosotros', component: NosotrosComponent},
-
-];
-
 import { BanerComponent } from './baner/baner.component';
-
-
-import { NavbarComponent } from './navbar/navbar.component';
-import { InicioComponent } from './inicio/inicio.component';
-/*ROUTING */
-import { Routes, RouterModule } from '@angular/router';
-import { NosotrosComponent } from './nosotros/nosotros.component';
-
 
 /*ROUTING */
 const appRoutes: Routes =[
